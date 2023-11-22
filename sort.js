a1761fd983a19e12d282a2118d03f9ec84bf23c4
atiboy-fr <ataullahabdullah181@gmail.com>
2023-11-22 14:27:27 +0100

    added comments

--- sort.js
+++ sort.js
@@ -1,11 +1,18 @@
+// Function to perform selection sort on an array
 function insertionSort(arr) {
+    // Iterate through each element in the array
     for(let i = 0; i < arr.length; i++) {
+        // Assume the current index is the index of the minimum element
         let lowestIndex = i
+        // Find the index of the minimum element in the unsorted part of the array
         for(let j = i + 1; j < arr.length; j++) {
+            // If the element at index j is less than the element at the assumed minimum index
             if(arr[j] < arr[lowestIndex]) {
+                // Update the index of the minimum element
                 lowestIndex = j;
             }
         }
+        // Swap the found minimum element with the first element of the unsorted part
         if(lowestIndex !== i) {
             let temp = arr[i];
             arr[i] = arr[lowestIndex];
